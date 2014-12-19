bc2412b19a2da8c7d48c3e6a418979610628aebf
Elena S Ackley <esa@ackleyshack.com>
2014-12-19 15:17:40 -0700

    Element Parameter member name code gen for list of Ints

--- ulam1/src/test/safe/t3217_test_compiler_elementandquark_elementparameterquark.cpp
+++ ulam1/src/test/safe/t3217_test_compiler_elementandquark_elementparameterquark.cpp
@@ -6,28 +6,30 @@ namespace MFM {
   {
     std::string GetAnswerKey()
     {
-      //Ue_Foo { System s();  Bool(7) sp(false);  Int(32) a(0);  Int(32) test() {  Foo f;  f chance . 1 cast = s ( chance )print . chance return } }\nExit status: 0
+      /* gen code output:
+	 Bool(3) Arg: 0x7 (true)
+      */
       return std::string("Ue_Foo { Bool(3) b(true);  System s();  Bool(1) sp(false);  Bool(3) c(false);  Bool(1) last(false);  Int(32) test() {  barchance valb 1 [] . true cast = b barchance valb 1 [] . cast = s ( b )print . b cast return } }\nExit status: 1");
     }
-    
+
     std::string PresetTest(FileManagerString * fms)
     {
       bool rtn1 = fms->add("Foo.ulam","ulam 1;\nuse System;\nuse Bar;\nelement Foo {\nSystem s;\nBool(1) sp;\nBool(3) b,c;\n element Bar barchance;\nBool last;\nInt test() {\nbarchance.valb[1] = true;\nb = barchance.valb[1];\ns.print(b);\nreturn b;\n }\n }\n");
-      
+
       bool rtn2 = fms->add("Bar.ulam"," ulam 1;\n quark Bar {\n Bool valb[3];\n  Void reset(Bool b) {\n b = 0;\n }\n }\n");
 
       // test system quark with native overloaded print funcs; assert
-      bool rtn3 = fms->add("System.ulam", "ulam 1;\nquark System {\nVoid print(Unsigned arg) native;\nVoid print(Int arg) native;\nVoid print(Int(4) arg) native;\nVoid print(Int(3) arg) native;\nVoid print(Unary(3) arg) native;\nVoid print(Bool(3) arg) native;\nVoid assert(Bool b) native;\n}\n");     
+      bool rtn3 = fms->add("System.ulam", "ulam 1;\nquark System {\nVoid print(Unsigned arg) native;\nVoid print(Int arg) native;\nVoid print(Int(4) arg) native;\nVoid print(Int(3) arg) native;\nVoid print(Unary(3) arg) native;\nVoid print(Bool(3) arg) native;\nVoid assert(Bool b) native;\n}\n");
 
       if(rtn1 && rtn2 && rtn3)
 	return std::string("Foo.ulam");
-      
+
       return std::string("");
     }
   }
-  
+
   ENDTESTCASECOMPILER(t3217_test_compiler_elementandquark_elementparameterquark)
-  
+
 } //end MFM
 
 
---
+++ ulam1/src/test/safe/t3260_test_compiler_element_elementparameterintlist.cpp
@@ -0,0 +1,37 @@
+#include "TestCase_EndToEndCompiler.h"
+
+namespace MFM {
+
+  BEGINTESTCASECOMPILER(t3260_test_compiler_element_elementparameterintlist)
+  {
+    std::string GetAnswerKey()
+    {
+      /* gen code output:
+	 Int Arg: 1
+	 Int Arg: 2
+	 Int Arg: 3
+      */
+
+      // element parameter chance not stored (as static variable) for eval
+      return std::string("Ue_Foo { Int(8) b(2);  System s();  Bool(7) sp(false);  Int(8) a(1);  Int(8) c(3);  Int(32) test() {  Foo f;  f chance . 1 cast = s ( chance )print . a f chance . cast = f choice . 2 cast = s ( choice )print . b f choice . cast = f result . 3 cast = s ( result )print . c f result . cast = f chance . return } }\nExit status: 3");
+    }
+
+    std::string PresetTest(FileManagerString * fms)
+    {
+      bool rtn1 = fms->add("Foo.ulam","ulam 1;\nuse System;\n element Foo {\nSystem s;\nBool(7) sp;\nelement Int chance, choice, result;\nInt(8) a, b, c;\n Int test() {\n Foo f;\nf.chance = 1;\ns.print(chance);\na = f.chance;\n f.choice = 2;\ns.print(choice);\nb = f.choice;\n f.result = 3;\ns.print(result);\nc = f.result;\nreturn f.chance;\n }\n }\n");
+
+      // test system quark with native overloaded print funcs; assert
+      bool rtn3 = fms->add("System.ulam", "ulam 1;\nquark System {\nVoid print(Unsigned arg) native;\nVoid print(Int arg) native;\nVoid print(Int(4) arg) native;\nVoid print(Int(3) arg) native;\nVoid print(Unary(3) arg) native;\nVoid print(Bool(3) arg) native;\nVoid assert(Bool b) native;\n}\n");
+
+      if(rtn1 && rtn3)
+	return std::string("Foo.ulam");
+
+      return std::string("");
+    }
+  }
+
+  ENDTESTCASECOMPILER(t3260_test_compiler_element_elementparameterintlist)
+
+} //end MFM
+
+
