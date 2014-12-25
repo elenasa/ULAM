7e8d6662918dd7227220034bc603be30696a01b9
Elena S Ackley <esa@ackleyshack.com>
2014-12-25 12:57:20 -0700

    Error msg for a quark defining an EP

--- ulam1/src/test/safe/t3233_test_compiler_quarkarray_ew.cpp
+++ ulam1/src/test/safe/t3233_test_compiler_quarkarray_ew.cpp
@@ -25,6 +25,9 @@ namespace MFM {
 */
       bool rtn2 = fms->add("EventWindow.ulam"," ulam 1;\n quark EventWindow {\nAtom aref(Int index) native;\nVoid aset(Int index, Atom v) native;\nInt test() {\nEventWindow ew;\nAtom a;\na=ew[0];\nreturn 0;\n}\n }\n");
 
+      //EventWindow.ulam:3:1: ERROR: Only elements may have element parameters: <EventWindow> is a quark.
+      //bool rtn2 = fms->add("EventWindow.ulam"," ulam 1;\n quark EventWindow {\nelement Int nogood;\n Atom aref(Int index) native;\nVoid aset(Int index, Atom v) native;\nInt test() {\nEventWindow ew;\nAtom a;\na=ew[0];\nreturn 0;\n}\n }\n");
+
       if(rtn2)
 	return std::string("EventWindow.ulam");
 
