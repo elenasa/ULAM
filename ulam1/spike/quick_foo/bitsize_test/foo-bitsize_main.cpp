0adbd6ddeb732102b8d4a4ecc5496f9060cc9195
Elena S Ackley <esa@ackleyshack.com>
2014-12-28 10:03:18 -0700

    bitsize_test updated (quick_foo)

--- ulam1/spike/quick_foo/bitsize_test/foo-bitsize_main.cpp
+++ ulam1/spike/quick_foo/bitsize_test/foo-bitsize_main.cpp
@@ -1,21 +1,20 @@
 /**                                        -*- mode:C++ -*/
 
-#include "../../include/itype.h"
+#include "itype.h"
 //#include "foo-bitsize_stat.h"
 #include "UlamBitsizeTest.h"
 
-#define RUNS 1000000
+#define RUNS 10000000
 #define ODDS 1000000
-#define TESTVAL 1
+#define MAXTESTVAL 7
 #define MODES 3
 
 int main()
 {
-
   for(MFM::u32 k = 0; k < MODES; k++)
     {
-      MFM::UlamBitsizeTest utest(ODDS, TESTVAL, (MFM::TestMode) k);  //Int, Unsigned, Unary
-      for(MFM::u32 j = 0; j < RUNS; j++)
+      MFM::UlamBitsizeTest utest(ODDS, MAXTESTVAL, (MFM::TestMode) k);  //Int, Unsigned, Unary
+      //for(MFM::u32 j = 0; j < RUNS; j++)
 	{
 	  utest.play();
 	}
