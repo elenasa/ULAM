1b90ef3b4470e97ca285567e0f86da2274ca3494
elenas <esa@ackleyshack.com>
2015-03-04 13:27:05 -0700

    check named constant and value types

--- ulam1/src/test/safe/t3342_test_compiler_elementandquarkswclassargs_typedeffromanotherclasstypedef.cpp
+++ ulam1/src/test/safe/t3342_test_compiler_elementandquarkswclassargs_typedeffromanotherclasstypedef.cpp
@@ -6,7 +6,7 @@ namespace MFM {
   {
     std::string GetAnswerKey()
     {
-      return std::string("Exit status: 3\nUe_P { Bool(1) b(false);  Int(32) test() {  3 = nvar const 3u cast return } }\nUq_Q { constant Int(CONSTANT) s = NONREADYCONST;  typedef Unsigned(UNKNOWN) Foo;  <NOMAIN> }\nUq_V { typedef Q(3) Woof;  <NOMAIN> }\n");
+      return std::string("Exit status: 3\nUe_P { Bool(1) b(false);  Int(32) test() {  3u = nvar const 3u cast return } }\nUq_Q { constant Int(CONSTANT) s = NONREADYCONST;  typedef Unsigned(UNKNOWN) Foo;  <NOMAIN> }\nUq_V { typedef Q(3) Woof;  <NOMAIN> }\n");
     }
 
     std::string PresetTest(FileManagerString * fms)
@@ -14,7 +14,7 @@ namespace MFM {
       //informed by t3339
       // recursive typedefs
       // must already be parsed!
-      bool rtn1 = fms->add("P.ulam","ulam 1;\nuse Q;\nuse V;\n element P {\nBool b;\nInt test() {\nconstant V.Woof.Foo nvar = 3;\n return nvar;\n}\n}\n");
+      bool rtn1 = fms->add("P.ulam","ulam 1;\nuse Q;\nuse V;\n element P {\nBool b;\nInt test() {\nconstant V.Woof.Foo nvar = 3u;\n return nvar;\n}\n}\n");
 
       bool rtn2 = fms->add("Q.ulam","ulam 1;\nquark Q(Int s) {\ntypedef Unsigned(s) Foo;\n}\n");
 
