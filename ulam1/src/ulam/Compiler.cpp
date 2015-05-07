08d5e6fd2d2404fee4ee3e9e1dc40289c8999844
elenas <esa@ackleyshack.com>
2015-05-07 15:17:57 -0600

    unseen class stays unknown size

--- ulam1/src/ulam/Compiler.cpp
+++ ulam1/src/ulam/Compiler.cpp
@@ -171,7 +171,7 @@ namespace MFM {
     u32 navcount = m_state.m_programDefST.countNavNodesAcrossTableOfClasses();
     if(navcount > 0)
       {
-	//assert(m_state.goAgain()); //sanity check; ran out of iterations
+	assert(m_state.goAgain()); //sanity check; ran out of iterations
 	std::ostringstream msg;
 	msg << navcount << " Nodes with illegal 'Nav' types detected after type labeling class: ";
 	msg << m_state.getUlamTypeNameBriefByIndex(m_state.getCompileThisIdx()).c_str();
