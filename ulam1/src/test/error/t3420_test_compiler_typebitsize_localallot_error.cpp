2aa7a010ec1d9a397b32c101342d25d1a4ed9253
elenas <esa@ackleyshack.com>
2015-06-07 16:36:17 -0600

    cleanup

--- ulam1/src/test/error/t3420_test_compiler_typebitsize_localallot_error.cpp
+++ ulam1/src/test/error/t3420_test_compiler_typebitsize_localallot_error.cpp
@@ -7,14 +7,14 @@ namespace MFM {
     std::string GetAnswerKey()
     {
       /*
-	./C.ulam:4:2: ERROR: Type Bitsize specifier for base type: Int has a constant value of 34 that exceeds the maximum bitsize 32.
+	./C.ulam:5:2: ERROR: Type Bitsize specifier for base type: Int has a constant value of 65 that exceeds the maximum bitsize 64.
       */
       return std::string("Exit status: -1\n");
     }
 
     std::string PresetTest(FileManagerString * fms)
     {
-      bool rtn1 = fms->add("C.ulam","ulam 1;\n element C {\nInt test() {\n Int(34) noway;\n return -1;\n}\n }\n");
+      bool rtn1 = fms->add("C.ulam","ulam 1;\n element C {\nInt test() {\n Int(34) way;\n Int(65) noway;\n return -1;\n}\n }\n");
 
       if(rtn1)
 	return std::string("C.ulam");
