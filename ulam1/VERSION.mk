f7f4a393b6af8acf74f3086c85ad4f194759e3cc
Dave Ackley <ackley@ackleyshack.com>
2015-07-11 16:31:37 -0600

    Improve doc

--- ulam1/VERSION.mk
+++ ulam1/VERSION.mk
@@ -2,7 +2,9 @@
 # THE ULAM COMPILER VERSION IS DEFINED HERE
 #
 # Changes to this file should be committed alone, and then HEAD should
-# be tagged like this: git tag -a -m "Tagging v1.0.0" v1.0.0
+# be tagged, and the tag pushed, something like this:
+#$ git tag -a -m "Tagging v1.0.0" v1.0.0
+#$ git push origin v1.0.0
 
 ULAM_VERSION_MAJOR:=1
 ULAM_VERSION_MINOR:=0
