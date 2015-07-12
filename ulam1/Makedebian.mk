ce8ef620e25885a72ce1ae4e13669de54a2a08e4
Dave Ackley <ackley@ackleyshack.com>
2015-07-12 08:34:26 -0600

    Begin debian packaging dev

---
+++ ulam1/Makedebian.mk
@@ -0,0 +1,20 @@
+# Extra stuff for debian install
+# Note this is meant to be run as 'make -C Makedebian.mk install'!
+DEB_PROGRAMS_TO_INSTALL += ulam culam
+DEB_PROGRAMS_PATHS_TO_INSTALL := $(DEB_PROGRAMS_TO_INSTALL:%=bin/%)
+DEB_ULAM_BINDIR := $(DESTDIR)/usr/bin
+DEB_ULAM_SHAREDIR := $(DESTDIR)/usr/share/ulam/
+
+# We're recursing rather than depending on 'all' so that the
+# $(PLATFORMS) mechanism doesn't need to know about install.
+install:	FORCE
+	COMMANDS=1 ULAM_SHARE_DIR=$(DEB_ULAM_SHAREDIR) make -k all
+	mkdir -p $(DEB_ULAM_BINDIR)
+	cp -a $(DEB_PROGRAMS_PATHS_TO_INSTALL) $(DEB_ULAM_BINDIR)
+	mkdir -p $(DEB_ULAM_SHAREDIR)
+	cp -r share/* $(DEB_ULAM_SHAREDIR)
+	# MAN AND DOC?
+
+include VERSION.mk
+version:	FORCE
+	@echo $(ULAM_VERSION_NUMBER)
