c71946ad81e20758a8de704b85ec4b8e5bea1a6d
Dave Ackley <ackley@ackleyshack.com>
2015-07-12 23:58:34 -0600

    Rotate the pod bays doors Hal

--- ulam1/VERSION.mk
+++ ulam1/VERSION.mk
@@ -13,3 +13,14 @@ ULAM_VERSION_REV:=0
 ################## NOTHING BELOW HERE SHOULD NEED TO CHANGE ##################
 
 ULAM_VERSION_NUMBER:=$(ULAM_VERSION_MAJOR).$(ULAM_VERSION_MINOR).$(ULAM_VERSION_REV)
+
+# Get the repo version (and save it for possible non-git-repo builds downstream)
+HAVE_GIT_DESCRIBE:=$(shell cd $(ROOT_DIR) && git describe 2>&1 >/dev/null && echo $$?)
+ifeq ($(HAVE_GIT_DESCRIBE),0)
+  ULAM_TREE_VERSION:=$(shell cd $(ROOT_DIR) && git describe)
+  $(shell echo "ULAM_TREE_VERSION:=$(ULAM_TREE_VERSION)" > TREEVERSION.mk)
+else
+  ULAM_TREE_VERSION:=unknown-rev
+  -include TREEVERSION.mk
+endif
+export ULAM_TREE_VERSION
