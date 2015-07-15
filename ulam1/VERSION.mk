e3dffe0f1e9df605a1d4f4a9092fa0833fe19c12
Dave Ackley <ackley@ackleyshack.com>
2015-07-14 18:06:15 -0600

    Rename ROOT_DIR to ULAM_ROOT_DIR

--- ulam1/VERSION.mk
+++ ulam1/VERSION.mk
@@ -15,23 +15,23 @@ ULAM_VERSION_REV:=0
 ULAM_VERSION_NUMBER:=$(ULAM_VERSION_MAJOR).$(ULAM_VERSION_MINOR).$(ULAM_VERSION_REV)
 
 # Get root dir now if we don't have it yet
-ifeq ($(ROOT_DIR),)
-ROOT_DIR := $(shell pwd)
-export ROOT_DIR
+ifeq ($(ULAM_ROOT_DIR),)
+ULAM_ROOT_DIR := $(shell pwd)
+export ULAM_ROOT_DIR
 endif
 
 # Suck in a git rep marker if it's been cached
 ULAM_TREE_VERSION:=unknown-rev
--include $(ROOT_DIR)/ULAM_TREEVERSION.mk
+-include $(ULAM_ROOT_DIR)/ULAM_TREEVERSION.mk
 
 # If our dir is writable, and we have git, and there's a repo tag,
 # that means we are in the ULAM_REPO_BUILD_TIME era, so we should cache
 # the tag for use in later eras.
-SHOULD_CACHE_REPO_TAG_ULAM:=$(shell test -w $(ROOT_DIR) && which git >/dev/null && cd $(ROOT_DIR) && git describe >/dev/null 2>&1 && echo YES)
-#${info AT<<$(realpath $(ROOT_DIR))>>=($(SHOULD_CACHE_REPO_TAG_ULAM))}
+SHOULD_CACHE_REPO_TAG_ULAM:=$(shell test -w $(ULAM_ROOT_DIR) && which git >/dev/null && cd $(ULAM_ROOT_DIR) && git describe >/dev/null 2>&1 && echo YES)
+#${info AT<<$(realpath $(ULAM_ROOT_DIR))>>=($(SHOULD_CACHE_REPO_TAG_ULAM))}
 ifeq ($(SHOULD_CACHE_REPO_TAG_ULAM),YES)
-  ULAM_TREE_VERSION:=$(shell cd $(ROOT_DIR) && git describe)
-  $(shell echo "ULAM_TREE_VERSION:=$(ULAM_TREE_VERSION)" > $(ROOT_DIR)/ULAM_TREEVERSION.mk)
+  ULAM_TREE_VERSION:=$(shell cd $(ULAM_ROOT_DIR) && git describe)
+  $(shell echo "ULAM_TREE_VERSION:=$(ULAM_TREE_VERSION)" > $(ULAM_ROOT_DIR)/ULAM_TREEVERSION.mk)
 else
 endif
 export ULAM_TREE_VERSION
