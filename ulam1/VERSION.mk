ca7ebb428aa63777d090d967015da179a66f1fea
Dave Ackley <ackley@ackleyshack.com>
2015-07-16 09:51:09 -0600

    Another version for launchpad

--- ulam1/VERSION.mk
+++ ulam1/VERSION.mk
@@ -4,11 +4,12 @@
 # Changes to this file should be committed alone, and then HEAD should
 # be tagged, and the tag pushed, something like this:
 #$ git tag -a -m "Tagging v1.0.0" v1.0.0
+#$ git push
 #$ git push origin v1.0.0
 
 ULAM_VERSION_MAJOR:=1
 ULAM_VERSION_MINOR:=0
-ULAM_VERSION_REV:=1
+ULAM_VERSION_REV:=2
 
 ################## NOTHING BELOW HERE SHOULD NEED TO CHANGE ##################
 
