470610b4cfb16ed687c822faefcbffc17df77a44
Dave Ackley <ackley@ackleyshack.com>
2015-07-15 19:06:05 -0600

    Do extract top Makefile doh

--- ulam1/share/perl/extractDistro.pl
+++ ulam1/share/perl/extractDistro.pl
@@ -35,6 +35,7 @@ my %categories = (
     "ULAM_shared_files"=> ["ULAM", "src", "find share"],
 
     "ULAM_packaging" =>   ["TOP", "src", "find debian"],
+    "ULAM_topmakefile" => ["TOP", "src", "ls -1 Makefile"],
     "ULAM_doc" =>         ["TOP", "src", "ls -1 *.md"],
     );
 
