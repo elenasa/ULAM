868f38f2a78b2772e8aeb97b514c306e8c2d6198
Dave Ackley <ackley@ackleyshack.com>
2015-07-16 22:58:02 -0600

    Log path for ulamexports

--- ulam1/share/perl/ReleaseDriverULAM.pl
+++ ulam1/share/perl/ReleaseDriverULAM.pl
@@ -115,7 +115,7 @@ sub REPO_BUILD {
     print "OK\n";
 
     print "Compiling standard elements..";
-    $ret = `cd ULAM/ulam1 && make ulamexports >../logs/REPO_BUILD_EXPORTS.log 2>&1 || echo -n \$?`;
+    $ret = `cd ULAM/ulam1 && make ulamexports >../../logs/REPO_BUILD_EXPORTS.log 2>&1 || echo -n \$?`;
     return "Repo .so build failed ($ret)"
         unless $ret eq "";
     print "OK\n";
