d262442db3dc5dfec3634be70da1cd0f2deafc11
elenas <esa@ackleyshack.com>
2017-01-17 10:35:51 -0700

    Issue RESOLVED: assign to a custom array class' data member

--- include/UlamType.h
+++ include/UlamType.h
@@ -163,6 +163,8 @@ namespace MFM{
 
     static ULAMTYPECOMPARERESULTS compareForString(UTI u1, CompilerState& state);
 
+    static ULAMTYPECOMPARERESULTS compareForCustomArrayItem(UTI u1, UTI u2, CompilerState& state);
+
     /** Number of bits (rounded up to nearest 32 bits) required to
     hold the total bit size  */
     u32 getTotalWordSize();
