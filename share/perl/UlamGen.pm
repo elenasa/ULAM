87e9a7b971510465b82c1285a11c6d1a3f5c4403
Dave Ackley <ackley@livingcomputation.org>
2021-10-10 17:04:01 -0600

    Tired much?

--- share/perl/UlamGen.pm
+++ share/perl/UlamGen.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 #202110101643 diagnostics is messing up with appimage-builder due to something causing
 # 'couldn't find diagnostic data' so screw it
-use diagnostics;
+#use diagnostics;
 use Carp;
 
 our $VERSION = 1.002;
